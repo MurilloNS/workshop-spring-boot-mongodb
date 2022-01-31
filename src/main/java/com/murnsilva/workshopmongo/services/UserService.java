3e2ceef87799840adde09088b5e0a22ae7d78e1e
MurilloNS <murillo.murillo@gmail.com>
2022-01-31 20:10:59 -0300

    Obtendo um usuário por id

--- src/main/java/com/murnsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/murnsilva/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.murnsilva.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.murnsilva.workshopmongo.domain.User;
 import com.murnsilva.workshopmongo.repository.UserRepository;
+import com.murnsilva.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findall(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encntrado!"));
 	}
 }
